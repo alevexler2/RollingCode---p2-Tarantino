2641e95008718b293c2ef1b009c58019df7c0263
alevexler <alevexler2@gmail.com>
2022-05-01 21:02:34 -0300

    modify meta tags on the index page

--- script/app.js
+++ script/app.js
@@ -15,7 +15,4 @@ btnLogin.addEventListener("click", () => {
   }
 });
 
-window.addEventListener("load", () => {
-  window.location == "http://127.0.0.1:5500/pages/loadLogin.html"? setTimeout(() => {window.location = "http://127.0.0.1:5500/index.html";}, 1500): "";
-});
 
