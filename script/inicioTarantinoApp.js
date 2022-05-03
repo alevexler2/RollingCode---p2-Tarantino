a8f7a9175fd755f8d56ea8edf34c3a318394f5cb
alevexler <alevexler2@gmail.com>
2022-05-03 19:24:28 -0300

    git merge with load-branch

--- script/inicioTarantinoApp.js
+++ script/inicioTarantinoApp.js
@@ -1,4 +1,35 @@
-const bgTarantino = document.querySelector("#bgTarantino");
+const bgTarantino = document.querySelector(".bgTarantino");
 setTimeout(() => {
-  bgTarantino.innerHTML = "Hola";
-}, 2000);
+  bgTarantino.innerHTML = `  <section class="container animate__animated animate__fadeIn" id="loginContainer">
+  <div class="d-flex justify-content-center align-items-center container mb-0 flex-column ">
+    <h1 class="text-white d-flex justify-content-center align-items-end logoStyle">Tarantino</h1>
+    <form>
+      <div class="mb-3">
+        <input type="text" class="form-control inputStyle" id="inputUser" placeholder="Usuario o Email" required>
+      </div>
+      <div class="mb-3">
+        <input type="password" class="form-control inputStyle" id="inputPass" placeholder="Contraseña" required>
+      </div>
+    </form>
+    <button type="button" class="btnUser m-0 text-center" id="btnLogin">INGRESAR</button>
+  </div>    
+</section>`;
+const indexContainer = document.querySelector("#loginContainer");
+const inputUser = document.querySelector("#inputUser");
+const inputPass = document.querySelector("#inputPass");
+const btnLogin = document.querySelector("#btnLogin");
+const user = "alejandro";
+const pass = "12345678";
+
+btnLogin.addEventListener("click", () => {
+  if (inputUser.value == "" || inputPass.value == "") {
+    alert("el usuario o la contraseña no pueden estar vacios");
+  } else if (inputUser.value == user && inputPass.value == pass) {
+    window.location = "http://127.0.0.1:5500/pages/loadLogin.html";
+  } else {
+    alert("el usuario o la contraseña son incorrectos");
+  }
+});
+}, 3000);
+
+
